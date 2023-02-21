5ed9a451504cb03eae29bef8405c45d714a693d3
celeste <parafactual@gmail.com>
2023-02-20 23:07:57 -0800

    wijufdhbgd

--- main.ts
+++ main.ts
@@ -1,137 +1,175 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+  App,
+  Editor,
+  ItemView,
+  MarkdownView,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+  WorkspaceLeaf,
+} from "obsidian";
+import { Configuration, OpenAIApi } from "openai";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface GPT3Settings {
+  apiKey: string;
+  model: string;
+  maxTokens: number;
+  nodesFolder: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: GPT3Settings = {
+  apiKey: "",
+  model: "davinci",
+  maxTokens: 8,
+  nodesFolder: "nodes",
+};
+
+export default class GPT3Plugin extends Plugin {
+  settings: GPT3Settings;
+  openai: OpenAIApi;
+
+  async onload() {
+    await this.loadSettings();
+
+    this.addSettingTab(new GPT3SettingTab(this.app, this));
+
+    const configuration = new Configuration({
+      apiKey: this.settings.apiKey,
+    });
+    this.openai = new OpenAIApi(configuration);
+
+    this.addCommand({
+      id: "gpt-3-complete",
+      name: "Complete",
+      icon: "wand",
+      editorCallback: async (editor: Editor, view: MarkdownView) =>
+        this.complete(this.settings.model, this.settings.maxTokens, editor),
+    });
+
+    this.registerView(
+      "loom",
+      (leaf) => new LoomView(leaf),
+    );
+    const loomExists = this.app.workspace.getLeavesOfType("loom").length > 0;
+    if (!loomExists)
+      this.app.workspace.getRightLeaf(false).setViewState({
+        type: "loom",
+      });
+  }
+
+  async complete(model: string, maxTokens: number, editor: Editor) {
+    let prompt;
+
+    const selection = editor.getSelection();
+    if (selection) prompt = selection;
+    else
+      prompt = (() => {
+        const cursorPosition = editor.getCursor();
+        const text = editor.getValue().split("\n");
+
+        let prompt = text.slice(0, cursorPosition.line + 1);
+        prompt[cursorPosition.line] = prompt[cursorPosition.line].slice(
+          0,
+          cursorPosition.ch
+        );
+        return prompt.join("\n");
+      })();
+
+    const trailingSpace = prompt.match(/\s+$/);
+    prompt = prompt.replace(/\s+$/, "");
+
+    let completion = (
+      await this.openai.createCompletion({
+        model,
+        prompt,
+        max_tokens: maxTokens,
+        temperature: 1,
+      })
+    ).data.choices[0].text;
+    if (trailingSpace && completion[0] === " ")
+      completion = completion.slice(1);
+
+    editor.replaceSelection(selection + completion);
+  }
+
+  onunload() {}
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class LoomView extends ItemView {
+  constructor(leaf: WorkspaceLeaf) {
+    super(leaf);
 
-	async onload() {
-		await this.loadSettings();
+    this.containerEl.empty();
+  }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+  getViewType(): string {
+    return "loom";
+  }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+  getDisplayText(): string {
+    return "Loom";
+  }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  getIcon(): string {
+    return "network";
+  }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class GPT3SettingTab extends PluginSettingTab {
+  plugin: MyPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  constructor(app: App, plugin: MyPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+  display(): void {
+    const { containerEl } = this;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+    containerEl.empty();
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    new Setting(containerEl)
+      .setName("OpenAI API key")
+      .setDesc("Required")
+      .addText((text) =>
+        text.setValue(this.plugin.settings.apiKey).onChange(async (value) => {
+          this.plugin.settings.apiKey = value;
+          await this.plugin.saveSettings();
+        })
+      );
 
-	display(): void {
-		const {containerEl} = this;
+    new Setting(containerEl).setName("Model").addText((text) =>
+      text.setValue(this.plugin.settings.model).onChange(async (value) => {
+        this.plugin.settings.model = value;
+        await this.plugin.saveSettings();
+      })
+    );
 
-		containerEl.empty();
+    new Setting(containerEl).setName("Length (in tokens)").addText((text) =>
+      text
+        .setValue(this.plugin.settings.maxTokens.toString())
+        .onChange(async (value) => {
+          this.plugin.settings.maxTokens = parseInt(value);
+          await this.plugin.saveSettings();
+        })
+    );
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl).setName("Nodes folder").addText((text) =>
+      text
+        .setValue(this.plugin.settings.nodesFolder)
+        .onChange(async (value) => {
+          this.plugin.settings.nodesFolder = value;
+          await this.plugin.saveSettings();
+        })
+    );
+  }
 }
